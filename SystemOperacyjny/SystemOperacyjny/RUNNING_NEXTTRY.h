146baae3afce43c1faf81bc8dd5ba7e5912214f3
michal93cz <michal.czupryniak@gmail.com>
2014-12-12 20:26:53 +0100

    usuniecie niepotrzenych headerów

--- SystemOperacyjny/SystemOperacyjny/RUNNING_NEXTTRY.h
+++
@@ -1,9 +0,0 @@
-#ifndef run_nexttry_h
-#define run_nexttry_h
-#include "PCB_class.h"
-
-
-__declspec(selectany) PCB *RUNNING = new PCB(); //Tells the compiler that the declared global data item
-__declspec(selectany) PCB *NEXTTRY = new PCB();
-
-#endif
