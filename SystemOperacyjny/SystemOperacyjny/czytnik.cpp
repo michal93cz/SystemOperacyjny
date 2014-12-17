7ee12b2dd36b2dc6d76ef1671a3c446309db67a1
Phi Long <phi.nguyen@student.put.poznan.pl>
2014-12-18 00:20:26 +0100

    Zmiana warunkow w czytniku - nic nie znaczaca

--- SystemOperacyjny/SystemOperacyjny/czytnik.cpp
+++ SystemOperacyjny/SystemOperacyjny/czytnik.cpp
@@ -9,31 +9,39 @@ vector <Bufor> Czyt::Czytaj(bool dr_nr, string com)
 {
 	JOB job;
 	string buf;
+	int licznik = 0;
 	vector <string> tmp;
 	vector <Bufor> bufor;
 	if (dr_nr == 1)
 	{
-		job.JOB_plik("plik1.job");
-		buf = job.getJOB(0);
-		tmp = split(buf, ' ');
-		if (tmp[0] != "$JOB"){
-			cout << "Karta jest nieprawidlowa";
+		if (USER_SEM->GET_VALUE() != 0)
+		{
+			cout << "Urzadzenie: Czytnik jest zajete";
 		}
-		else{
-			if (com != "READ"){
+		else
+		{
+			if (com != "READ")
+			{
 				cout << "Odebrano zly komunikat";
 			}
-			else{
-				string rozkazy1;
-				if (USER_SEM->GET_VALUE() != 0)
+			else
+			{
+				job.JOB_plik("plik1.job");
+				USER_SEM->P();
+				for (int i = 0; i < job.nrlini; i++)
 				{
-					cout << "Urzadzenie: Czytnik jest zajete";
-				}
-				else
-				{
-					USER_SEM->P();
-					cout << "Rozpoczecie czytania" <<endl;
-					for (int i = 0; i <job.nrlini; i++){
+					buf = job.getJOB(i);
+					tmp = split(buf, ' ');
+					if (tmp[0] != "$JOB")
+					{
+						cout << "Nieprawidlowa karta";
+
+					}
+					else
+					{
+
+						cout << "Rozpoczecie czytania:" << endl;
+
 						bufor.push_back(Bufor());
 						bufor[i].rozkazy = job.getData(i);
 						bufor[i].nazwa = job.getName(i);
@@ -44,45 +52,52 @@ vector <Bufor> Czyt::Czytaj(bool dr_nr, string com)
 				}
 				USER_SEM->V();
 			}
+
 		}
+
 	}
 	else if (dr_nr == 0)
 	{
-
-		job.JOB_plik("plik2.job");
-		buf = job.getJOB(0);
-		tmp = split(buf, ' ');
-		if (tmp[0] != "$JOB"){
-			cout << "Karta jest nieprawidlowa";
+		if (USER_SEM->GET_VALUE() != 0)
+		{
+			cout << "Urzadzenie: Czytnik jest zajete";
 		}
-		else{
-
-			string rozkazy2;
-			if (USER_SEM->GET_VALUE() != 0)
+		else
+		{
+			if (com != "READ")
 			{
-				cout << "Urzadzenie: Czytnik jest zajete";
+				cout << "Odebrano zly komunikat";
 			}
 			else
 			{
-				if (com != "READ"){
-					cout << "Odebrano zly komunikat";
-				}
-				else{
+				job.JOB_plik("plik2.job");
 				USER_SEM->P();
-				cout << "Rozpoczecie czytania" << endl;
-				for (int i = 0; i < job.nrlini; i++){
-					bufor.push_back(Bufor());
-					bufor[i].rozkazy = job.getData(i);
-					bufor[i].nazwa = job.getName(i);
-					bufor[i].size = job.getSize(i);
-					bufor[i].inout = job.sprawdzIO(i);
+				for (int i = 0; i < job.nrlini; i++)
+				{
+					buf = job.getJOB(i);
+					tmp = split(buf, ' ');
+					if (tmp[0] != "$JOB")
+					{
+						cout << "Nieprawidlowa karta";
+
+					}
+					else
+					{
+
+						cout << "Rozpoczecie czytania:" << endl;
+
+						bufor.push_back(Bufor());
+						bufor[i].rozkazy = job.getData(i);
+						bufor[i].nazwa = job.getName(i);
+						bufor[i].size = job.getSize(i);
+						bufor[i].inout = job.sprawdzIO(i);
+					}
 				}
-				
-			}
+
 			}
 			USER_SEM->V();
+
 		}
 	}
 	return bufor;
 }
-
